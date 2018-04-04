18832337df412eb1d0eb78c79d631ccb30796775
Evan B <bel190604@gmail.com>
2018-04-04 15:39:30 -0400

    gameegnine
    Added code for android game engine.

--- src/myfirstandroidgame/SimpeGameEngine.java
+++ src/myfirstandroidgame/SimpeGameEngine.java
@@ -4,6 +4,31 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 public class SimpeGameEngine {
-
+// gameView will be the view of the game
+// It will also hold the logic of the game
+// and respond to screen touches s well
+	GameView gameView;
+	
+	@Override
+	protected void onCreate(bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		 
+        // Initialize gameView and set it as the view
+        gameView = new GameView(this);
+        setContentView(gameView);
+	}
+	//Gameview will go here 
+	
+	//more SimpleGameEngine methods will go here
+	class GameView extends SurfaceView implements Runable{
+		
+	}
 }
